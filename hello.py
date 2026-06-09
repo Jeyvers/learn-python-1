1fda41723edd070dd28755a14eff2594f71eeb69
Jeyi Adole <meetjeyvers@gmail.com>
2026-06-09 16:27:19 +0100

    initial commit

---
+++ hello.py
@@ -0,0 +1,26 @@
+# print("Hello, World!")
+# of course -- it's necessary when learning a new language
+
+
+#  eh well, don't judge me
+
+# escape characters, we don't know that
+
+# Variables
+# we know what variables are.
+
+# Other inbuilt functions
+name = input("Enter your name: ")
+# for numbers, you gotta wrap them in int() for bit_length() to work. Else, use len()
+age = int(input("Enter your age"))
+name.upper()
+print(type(name))  # to get the type of the variable
+print(name)
+# for length it's len for sting, bit_length for numbers
+print(len(name), age.bit_length())
+
+'hello'.capitalize()
+print("hello".upper())
+
+# data types
+# will there be anything new? eh well, usuals: int, string, boolean, float, list (array), set, tuple,. dict (object), nonetype?
